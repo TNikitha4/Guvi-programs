3bdd16a025f73f43eb739e69eb187c650affcce2
TNikitha4 <46804949+TNikitha4@users.noreply.github.com>
2019-06-05 21:39:17 -0700

    Create hunter13.py

---
+++ hunter13.py
@@ -0,0 +1,13 @@
+#nikitha
+inp1=int(input())
+inp2=input().split()
+l=[]
+for i in range(0,inp1):
+  if(int(inp2[i])==i):
+    l.append(inp2[i])
+if(l==[]):
+  print("-1")
+else:
+  l.sort()
+  for j in range(0,len(l)):
+    print(l[j],end=" ")
