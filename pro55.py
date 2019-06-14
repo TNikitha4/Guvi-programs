e9cf0e1e06497e64672cd745370597bd4d9860a5
TNikitha4 <46804949+TNikitha4@users.noreply.github.com>
2019-06-14 16:08:23 +0530

    Create pro55.py

---
+++ pro55.py
@@ -0,0 +1,16 @@
+#nikitha
+n=input()
+if n==n[::-1]:
+    print("yes")
+else:
+    value=n.strip("0")
+    
+    if value==value[::-1]:
+        print("yes")
+    else:
+        value=n.lstrip("0")
+        
+        if value==value[::-1]:
+            print("yes")
+        else:
+            print("no")
